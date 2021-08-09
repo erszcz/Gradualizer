106a9f00555c42f98536234d2dddf4e481bc6d56
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2021-08-09 22:34:26 +0200

    wip: Fix rule 2 breaking a/test/should_pass/map_field_valid_update:i/1

--- src/typechecker.erl
+++ src/typechecker.erl
@@ -270,20 +270,27 @@ compat_ty({type, _, map, Assocs1}, {type, _, map, Assocs2}, A, TEnv) ->
     %% for the below rule definitions:
     %% 2. For all mandatory associations K2 := V2 in M2,
     %% there is a mandatory association K1 := V1 in M1...
-    %IsMandatory = fun
-    %                  ({type, _, map_field_exact, _}) -> true;
-    %                  (_) -> false
-    %              end,
-    %Mandatory1 = lists:filter(IsMandatory, Assocs1),
-    %Mandatory2 = lists:filter(IsMandatory, Assocs2),
-    %lists:foreach(fun ({type, _, map_field_exact, _} = Assoc2) ->
-    %                      %% lists:foreach( any_type(), Mandatory1 ) combo will only throw nomatch,
-    %                      %% if there's at least one Mandatory1 association; let's make sure there is.
-    %                      length(Mandatory1) == 0 andalso throw(nomatch),
-    %                      lists:foreach(fun (Assoc1) ->
-    %                                            any_type(Assoc1, [Assoc2], A, TEnv)
-    %                                    end, Mandatory1)
-    %              end, Mandatory2),
+    IsMandatory = fun
+                      ({type, _, map_field_exact, _}) -> true;
+                      (_) -> false
+                  end,
+    Mandatory1 = lists:filter(IsMandatory, Assocs1),
+    Mandatory2 = lists:filter(IsMandatory, Assocs2),
+    lists:foreach(fun ({type, _, map_field_exact, _} = Assoc2) ->
+                          %% lists:foreach( any_type(), Mandatory1 ) combo will only throw nomatch,
+                          %% if there's at least one Mandatory1 association; let's make sure there is.
+                          length(Mandatory1) == 0 andalso throw(nomatch),
+                          NoMatch = lists:all(fun (Assoc1) ->
+                                                      try
+                                                          temp_name(Assoc1, [Assoc2], A, TEnv),
+                                                          false
+                                                      catch
+                                                          nomatch -> true
+                                                      end
+                                                      %any_type(Assoc1, [Assoc2], A, TEnv)
+                                              end, Mandatory1),
+                          NoMatch andalso throw(nomatch)
+                  end, Mandatory2),
     %% 1. For all associations K1 <Assoc1> V1 in M1,
     %% there exists an association K2 <Assoc2> V2 in M2...
     lists:foldl(fun (Assoc1, {As, Cs1}) ->
@@ -309,6 +316,8 @@ compat_ty({type, _, AssocTag1, [Key1, Val1]},
 compat_ty(_Ty1, _Ty2, _, _) ->
     throw(nomatch).
 
+temp_name(Assoc1, [Assoc2], A, TEnv) ->
+    any_type(Assoc1, [Assoc2], A, TEnv).
 
 compat_tys([], [], A, _TEnv) ->
     ret(A);

26b04738b42db1f50fdc73481c1a700cb5e8b5f9
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2021-09-15 22:18:12 +0200

    Specify comment in test/known_problems/should_pass/map_update2.erl

--- test/known_problems/should_pass/map_update2.erl
+++ test/known_problems/should_pass/map_update2.erl
@@ -12,7 +12,7 @@ mapup2(A, M1) ->
                    {M1#{b := 1}}
            end,
     %% inferred type of M2 is none()
-    %% because glb_ty gives up on non-trivial map types
+    %% because glb_ty cannot handle maps with overlapping keys
 
     %% update_map_type does not handle none()
     M2#{c := 1}.

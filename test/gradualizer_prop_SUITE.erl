61973a51b2c08a8f13c9a5c9799064f3473e7cf3
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2021-10-16 18:47:03 +0200

    Define the first property based test

---
+++ test/gradualizer_prop_SUITE.erl
@@ -0,0 +1,17 @@
+-module(gradualizer_prop_SUITE).
+
+-compile([export_all, nowarn_export_all]).
+
+all() ->
+    [
+     remove_pos_removes_pos
+    ].
+
+init_per_suite(Config) ->
+    ct_property_test:init_per_suite(Config).
+
+end_per_suite(Config) ->
+    Config.
+
+remove_pos_removes_pos(Config) ->
+    ct_property_test:quickcheck(gradualizer_prop:prop_remove_pos_removes_pos(), Config).

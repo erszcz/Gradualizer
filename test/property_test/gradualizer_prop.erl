ce735175ce1602fe6c0e01b11e8c19f671e79177
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2021-10-21 23:10:29 +0200

    Start work on gradualizer_type_gen:abstract_type/0

--- test/property_test/gradualizer_prop.erl
+++ test/property_test/gradualizer_prop.erl
@@ -15,11 +15,13 @@ is_atom_or_integer({integer, Int}) when is_integer(Int) -> true;
 is_atom_or_integer(_) -> false.
 
 
+abstract_type() ->
+    gradualizer_type_gen:abstract_type().
+
 prop_remove_pos_removes_pos() ->
-    ?FORALL(Type, gradualizer_type:abstract_type(),
-            ?IMPLIES(is_not_user_type(Type),
-                     ?WHENFAIL(ct:pal("~s failed:\n~p\n", [?FUNCTION_NAME, Type]),
-                               prop_remove_pos_removes_pos_(Type)))).
+    ?FORALL(Type, abstract_type(),
+            ?WHENFAIL(ct:pal("~s failed:\n~p\n", [?FUNCTION_NAME, Type]),
+                      prop_remove_pos_removes_pos_(Type))).
 
 prop_remove_pos_removes_pos_(Type) ->
     typelib:remove_pos(Type),
@@ -29,24 +31,24 @@ prop_remove_pos_removes_pos_(Type) ->
 
 prop_normalize_type() ->
     ?FORALL(Type,
-            gradualizer_type:abstract_type(),
+            abstract_type(),
             ?WHENFAIL(ct:pal("~s failed:\n~p\n", [?FUNCTION_NAME, Type]),
-                               ?TIMEOUT(timer:seconds(1),
-                                        begin
-                                            mock_type_in_gradualizer_db(Type),
-                                            prop_normalize_type_(Type)
-                                        end))).
+                      ?TIMEOUT(timer:seconds(1),
+                               begin
+                                   %mock_type_in_gradualizer_db(Type),
+                                   prop_normalize_type_(Type)
+                               end))).
 
 %% TODO: First, this only catches `user_type' on the top-level, not when it's generated
 %%       as a nested type.
 %%       Second, we should come up with something more clever to handle user type generation.
 %%       Maybe predefine the type in gradualizer_db in such a case?
-is_not_user_type({user_type, _, _, _}) -> false;
-is_not_user_type(_) -> true.
+%is_not_user_type({user_type, _, _, _}) -> false;
+%is_not_user_type(_) -> true.
 
-mock_type_in_gradualizer_db(Type) ->
-    ct:pal("~p: not_implemented_yet\n", [?FUNCTION_NAME]),
-    ok.
+%mock_type_in_gradualizer_db(Type) ->
+%    ct:pal("~p: not_implemented_yet\n", [?FUNCTION_NAME]),
+%    ok.
 
 prop_normalize_type_(Type) ->
     %% Try to make these rules easily copy-pastable to the Erlang shell,

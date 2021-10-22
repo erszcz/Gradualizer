62165dad2d3a43b7b13a45cbb5b316ae56113697
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2021-10-23 00:15:32 +0200

    Define prop_type_check_expr

--- test/property_test/gradualizer_type_gen.erl
+++ test/property_test/gradualizer_type_gen.erl
@@ -1,14 +1,22 @@
 -module(gradualizer_type_gen).
 
--export([abstract_type/0, abstract_type/1]).
+-export([abstract_type/0, abstract_type/1,
+         expr/0, expr/1]).
 
 abstract_type() ->
-    DefaultOpts = [{weight, {binop, 0}},
-                   {weight, {unop, 0}},
-                   {weight, {remote_type, 0}},
-                   {weight, {user_defined_type, 0}}],
-    abstract_type(DefaultOpts).
+    Opts = [{weight, {binop, 0}},
+            {weight, {unop, 0}},
+            {weight, {remote_type, 0}},
+            {weight, {user_defined_type, 0}}],
+    abstract_type(Opts).
 
 abstract_type(Opts) ->
     State = gradualizer_erlang_abstract_code:options(Opts),
     gradualizer_erlang_abstract_code:abstract_type(State).
+
+expr() ->
+    Opts = [],
+    expr(Opts).
+
+expr(Opts) ->
+    gradualizer_erlang_abstract_code:expr(Opts).

6da954e72827c3e673c27679511e47e39efe5730
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2022-04-05 21:27:44 +0200

    debug

--- src/gradualizer_app.erl
+++ src/gradualizer_app.erl
@@ -15,6 +15,18 @@
 %%%===================================================================
 
 start(_StartType, _StartArgs) ->
+
+    F = fun
+            (Trace, ok) ->
+                io:format("~p\n", [Trace])
+        end,
+    dbg:tracer(process, {F, ok}),
+    dbg:p(all, call),
+    %dbg:tpl(typechecker, expect_map_type, x),
+    dbg:tpl(typechecker, refine_list_ty, 2, x),
+    dbg:tpl(typechecker, refine_ty, x),
+    dbg:tpl(typechecker, add_type_pat, x),
+
     Opts = application:get_env(gradualizer, options, []),
     gradualizer_sup:start_link(Opts).
 

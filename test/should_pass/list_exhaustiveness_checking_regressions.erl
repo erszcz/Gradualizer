e90dc03c5f5d555cbabb0b708f384dbad196ff6f
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2022-04-06 18:45:18 +0200

    Add more list matching functions which should pass

--- test/should_pass/list_exhaustiveness_checking_regressions.erl
+++ test/should_pass/list_exhaustiveness_checking_regressions.erl
@@ -1,6 +1,6 @@
 -module(list_exhaustiveness_checking_regressions).
 
--export([f/2, g/1, h/1, i/1]).
+-export([f/2, g/1, h/1, i/1, j/1, k/1]).
 
 f( Needle, [Needle | _]) -> ok;
 f( Needle, [_ | Haystack]) -> f(Needle, Haystack);
@@ -20,3 +20,12 @@ h([_ | Haystack]) -> h(Haystack).
 i([]) -> ok;
 i([Cs]) -> ok;
 i([C1, C2 | Cs]) -> ok.
+
+-spec j([atom()]) -> integer().
+j([]) -> 1;
+j([C1, C2 | _]) -> 2;
+j([Cs]) -> 3.
+
+-spec k(list(atom())) -> integer().
+k([_, _ | _]) -> 1;
+k(_) -> 2.

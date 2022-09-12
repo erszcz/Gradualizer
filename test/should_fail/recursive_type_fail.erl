0771bb67b4f4e676d95f1ef96c0e7ea714be464f
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2022-09-12 11:29:50 +0200

    Extend test/should_fail/recursive_type_fail.erl

--- test/should_fail/recursive_type_fail.erl
+++ test/should_fail/recursive_type_fail.erl
@@ -1,13 +1,23 @@
 -module(recursive_type_fail).
 
--export([f/0]).
+-export([g/0, i/0, j/0]).
 
--type recursive_t() :: #{key => recursive_t()}.
+-type recursive_t1() :: #{key => recursive_t1()}.
 
--spec f() -> recursive_t().
+-type recursive_t2() :: #{binary() | atom() => recursive_t2()} | true | false | null.
+
+-spec f() -> recursive_t1().
 f() ->
     g().
 
--spec g() -> recursive_t().
+-spec g() -> recursive_t1().
 g() ->
+    [].
+
+-spec i() -> recursive_t2().
+i() ->
+    j().
+
+-spec j() -> recursive_t2().
+j() ->
     [].

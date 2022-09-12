f6befa2788b602e328839743a6b75afc88828e28
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2022-09-12 17:15:18 +0200

    dbg

--- src/gradualizer_app.erl
+++ src/gradualizer_app.erl
@@ -17,15 +17,23 @@
 start(_StartType, _StartArgs) ->
 
     F = fun
+            ({trace, _Pid, call, {_M, _F = do_type_check_expr_in, [_Env | _Args]}}, ok) ->
+                Trace = {trace, _Pid, call, {_M, _F, [env | _Args]}},
+                io:format("~p\n", [Trace]);
+            ({trace, _Pid, return_from, {_M, _F = do_type_check_expr_in, _Arity}, _RetVal}, ok) ->
+                Trace = {trace, _Pid, return_from, {_M, _F, _Arity}, {env, constraints}},
+                io:format("~p\n", [Trace]);
             (Trace, ok) ->
                 io:format("~p\n", [Trace])
         end,
-    %dbg:tracer(process, {F, ok}),
+    dbg:tracer(process, {F, ok}),
     dbg:p(all, call),
-    dbg:tpl(typechecker, expect_tuple_type, x),
-    dbg:tpl(typechecker, compat, x),
+    %dbg:tpl(typechecker, expect_tuple_type, x),
+    %dbg:tpl(typechecker, compat, x),
     %dbg:tpl(typechecker, compat_seen, x),
     %dbg:tpl(typechecker, compat_ty, x),
+    dbg:tpl(typechecker, debug, x),
+    dbg:tpl(typechecker, do_type_check_expr_in, x),
 
     Opts = application:get_env(gradualizer, options, []),
     gradualizer_sup:start_link(Opts).
